bc5f4ac34d062943c42502e778dde7907bc0bc64
apmfree78 <amit@profitswami.com>
2022-08-25 14:06:30 -0400

    refactored code and cleaned up a bit, plus improved algorithm speed for game simulation

--- src/App.js
+++ src/App.js
@@ -1,70 +1,69 @@
 import { useEffect, useRef, useState } from "react";
-import { chooseRandomWord, getWordsOfLength, winningWords } from './computer';
+import {
+  chooseRandomWord,
+  getLongestWords,
+  getValidLetters,
+  getWinningLetters,
+  mineWinningLetters
+} from './computer';
 import { playSound } from './library/sounds'
 import "./App.css";
 import { dictionary } from "./ghost";
 import trie from 'trie-prefix-tree';
 
 function App() {
-  let words = useRef();
   const [win, setWin] = useState(''); // message for who has won 
   const [isWinner, setIsWinner] = useState(false);
   const [turn, setTurn] = useState(true);  // toggle turn between human and computer
   const [matches, setMatches] = useState([]); // words that match input letters
   const [letters, setLetters] = useState(""); // string of input letters
+
+  //****************************************************************
+  //*********** PREFIX TREE OF DICTIONARY *********************
+  let prefixTree = useRef();
   // load words into trie when application first loads
   useEffect(() => {
-    words.current = trie(dictionary);
+    prefixTree.current = trie(dictionary);
   }, []);
+  //****************************************************************
+  //****************************************************************
 
-  // computer plays it's turn
+
+
+  // computer plays it's turn - see also useEffect [turn]
   function computerPlays() {
 
-    // determine if odds are in computers favor
-    // getting array of possible words from current prefix
-    const prefixWords = words.current.getPrefix(letters);
+    // USING RECURSIVE ALGO TO SIMULATE GAME PLAY
+    // AND FIND OPTIMAL LETTERS FOR COMPUTERS TO PLAY
+    const winningLetters = mineWinningLetters(letters, prefixTree.current);
+    console.log(`winning letters: ${winningLetters}`);
 
-    // if letters < 3 than just choose random word from prefix list
-    if (letters.length < 3) {
-      let randomWord = '';
-      while (randomWord.length <= letters.length) randomWord = chooseRandomWord(prefixWords);
-      // determine new letter and set state
-      const newLetter = randomWord[letters.length];
-      setLetters(letters + newLetter);
-      return newLetter;
-    }
-    // using winningWord method to determine if computer has
-    // favorable odds, return array of top words to choose from,
-    // and returning longest word as well
-    const [odds, winners, longest] = winningWords(prefixWords, letters.length);
+    // checking to see if recursive simulation found winning letters
+    // if so , randomly picking one
+    if (winningLetters) { // WIN SCENARIO
 
-    console.log(`odds are ${odds},longest word is of length ${longest}`);
-    console.log(`winners are ${winners}`);
-
-
-    if (odds) { // WIN SCENARIO
-
-
-      // we pull a random word that contains the prefix contained in 'letters'
-      // than the computer's letter will be the letters.length character of that word
-      const randomWord = chooseRandomWord(winners);
-      console.log('getting word');
-      console.log(randomWord);
-      // since gameLogic ran right before this function was called
-      // we know that this prefix is valid and contains words
-      // and it's not a completed word, otherwise game would be over
+      // choose a random letter from winningLetters
+      // generated above
+      const newLetter = chooseRandomWord(winningLetters);
+      console.log('getting letter');
+      console.log(newLetter);
       //setting state with new letter
-      const newLetter = randomWord[letters.length];
       setLetters(letters + newLetter);
       return newLetter;
     }
     else { // LOSE SCENARIO
 
-      // computer odds are 50% or less, so lets stretch out the game
+      // getting array of possible words from current prefix
+      const prefixWords = prefixTree.current.getPrefix(letters);
+
+      // computer odds are low, so lets stretch out the game
       // by picking from words of the longest length 
-      const LongestWords = getWordsOfLength(prefixWords, longest);
+      const LongestWords = getLongestWords(prefixWords);
+      console.log('longest words');
       console.log(LongestWords);
       const LongWord = chooseRandomWord(LongestWords);
+      if (LongWord.length <= letters.length) return '';
+
       const newLetter = LongWord[letters.length];
       // setting state 
       setLetters(letters + newLetter);
@@ -72,54 +71,54 @@ function App() {
     }
   }
 
+  // set winner of game
+  function declareWinner(winner) {
+
+    if (winner === 'pc') {
+      setWin('Computer has WON!');
+      setIsWinner(true);
+      playSound('lost')
+    } else {
+      setWin('Human Player has WON!');
+      setIsWinner(true);
+      playSound('win')
+    }
+  }
   // this function determins state of game
-  // based on letters imputed by user and computer
+  // based on letters imputed by user or computer
+  // if there is a winner, winner is set
+  // sample output of potential word matches
+  // is show on screen too
   function gameLogic(currentPlayer = 'human', sequence = letters) {
 
-    // if we have more than 3 sequence than check trie for a match
-    if (sequence.length > 3) {
-      // check if prefix exists
-      if (words.current.isPrefix(sequence)) {
-        //update matches
-        console.log(`${sequence} is valid`)
-        const _words = words.current.getPrefix(sequence);
-        setMatches([..._words]);
+    //check if valid prefix was submitted
+    const isValidPrefix = prefixTree.current.isPrefix(sequence);
+
+    if (isValidPrefix) {
+      //update matches
+      console.log(`${sequence} is valid`)
+      const _words = prefixTree.current.getPrefix(sequence);
+      setMatches([..._words]);
+
+      // if we have more than 3 sequence than check trie for a match
+      if (sequence.length > 3) {
+        // check if prefix exists
 
         //check if sequence form word, if so it's a losing move! 
 
-        if (words.current.hasWord(sequence)) {
+        if (prefixTree.current.hasWord(sequence)) {
           //WE GOT A WINNER!  
           console.log(`${sequence} is a word`)
-          if (currentPlayer === 'human') {
-            setWin('Computer has WON!');
-            setIsWinner(true);
-            playSound('lost')
-          } else {
-            setWin('Human Player has WON!');
-            setIsWinner(true);
-            playSound('win')
-          }
-        }
-      }
-      else { // there are no matching prefexes
-        // who ever is current player loses! no more matches
-        console.log('no prefixes found, game over');
-        if (currentPlayer === 'human') {
-          setWin('Computer has WON!');
-          setIsWinner(true);
-          playSound('lost')
-        } else {
-          setWin('Human Player has WON!');
-          setIsWinner(true);
-          playSound('win')
+          if (currentPlayer === 'human') declareWinner('pc');
+          else declareWinner('human');
         }
       }
     }
     else {
-      //clear matches if there are any
-      setMatches([]);
-      // restart game
-      setWin('');
+
+      console.log('no prefixes found, game over');
+      if (currentPlayer === 'human') declareWinner('pc');
+      else declareWinner('human');
     }
   }
 
@@ -155,7 +154,7 @@ function App() {
     const lastChar = currentInput[currentInput.length - 1];
 
     // set state if a valid character was submitted
-    if ((/[a-z]/i).test(lastChar)) setLetters(currentInput);
+    if ((/[a-z]/).test(lastChar)) setLetters(currentInput);
   };
 
   return (
